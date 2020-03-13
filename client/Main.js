7a13a9f5838c37cc73742f8da5593ea0cd2df7b0
Sylvan Swierkosz <sswierkosz@gmail.com>
2020-03-12 23:41:32 -0700

    no longer sets stats on interval

--- client/Main.js
+++ client/Main.js
@@ -8,7 +8,6 @@ import BottomBar from 'components/BottomBar'
 import Spinner from 'components/Spinner'
 import Stars from 'components/Stars'
 import DonateModal from 'components/DonateModal'
-import calculateInterval from './lib/calculateInterval'
 import numberWithCommas from './lib/numberWithCommas'
 import donate from './assets/donate.png'
 // import { TRANSACTIONS_SET_1, TRANSACTIONS_SET_2 } from './lib/testData'
@@ -30,24 +29,16 @@ export default function Main() {
     getLatest()
     setInterval(() => {
       getLatest()
-    }, 10000)
+    }, 7000)
   }, [])
 
   function getLatest() {
     return axios.get('/latest')
       .then(({data}) => {
         if (data.block.number !== currentBlock) {
-          const { dailyClauses, dailyTransactions, dailyVTHOBurn } = data.stats
-          const previousStats = { dailyClauses, dailyTransactions, dailyVTHOBurn }
-          previousStats.dailyTransactions -= data.transactions.length
-          data.transactions.forEach(transaction => {
-            previousStats.dailyVTHOBurn -= +transaction.vthoBurn
-            previousStats.dailyClauses -= +transaction.clauses
-          })
           currentBlock = data.block.number
-          setStats(previousStats)
+          setStats(data.stats)
           setBlock(data.block)
-          setStatsOnIntervals(data.stats, setStats, data.transactions)
           if (data.block.number % 4 === 1) {
             setTransactionsBlock1(data.transactions)
           } else if (data.block.number % 4 === 2) {
@@ -76,27 +67,4 @@ export default function Main() {
     <Transactions transactions={transactionsBlock4} />
     <DonateModal open={modalVisible} toggleModalVisibility={() => { toggleModalVisibility(!modalVisible) }} />
   </Div100vh>
-}
-
-function setStatsOnIntervals(stats, setStats, transactions) {
-  stats = {
-    dailyTransactions: stats.dailyTransactions - transactions.length,
-    dailyClauses: stats.dailyClauses - transactions.reduce((acc, cur) => acc + +cur.clauses,0),
-    dailyVTHOBurn: stats.dailyVTHOBurn - transactions.reduce((acc, cur) => acc + +cur.vthoBurn,0),
-  }
-  const interval = calculateInterval(transactions.length)
-  let clauses = 0
-  let vtho = 0
-  for (let index = 0; index < transactions.length; index++) {
-    const transaction = transactions[index]
-    setTimeout(() => {
-      clauses += +transaction.clauses
-      vtho += +transaction.vthoBurn
-      setStats({
-        dailyTransactions: stats.dailyTransactions + index + 1,
-        dailyClauses: stats.dailyClauses + clauses,
-        dailyVTHOBurn:  stats.dailyVTHOBurn + vtho
-      })
-    }, (interval * index) + 100)
-  }
 }
