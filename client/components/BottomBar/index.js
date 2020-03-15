7ec3145a40e477801301a5723d7acb11cf3d936c
Sylvan Swierkosz <sswierkosz@gmail.com>
2020-03-14 17:10:07 -0700

    bottom bar numbers flash yellow when they update

--- client/components/BottomBar/index.js
+++ client/components/BottomBar/index.js
@@ -1,24 +1,34 @@
 import React from 'react'
+import { useEffect, useState } from 'preact/hooks'
 
 import numberWithCommas from '../../lib/numberWithCommas'
 
 import './index.sass'
 
 export default function BottomBar({ stats }) {
+  const [numberClass, setNumberClass] = useState('BottomBar-number')
+  useEffect(() => {
+    setNumberClass('BottomBar-number BottomBar-number-changing')
+    setTimeout(() => {
+      setNumberClass('BottomBar-number')
+    }, 300)
+  }, [stats.dailyVTHOBurn])
+
   if (!stats || Object.keys(stats).length === 0) return <div className="BottomBar" />
+
   return <div className="BottomBar">
     <div className="BottomBar-wrapper">
       <div>
         <span className="BottomBar-header">VTHO Burn: </span>
-        {numberWithCommas(Math.floor(+stats.dailyVTHOBurn))}
+        <span className={numberClass}>{numberWithCommas(Math.floor(+stats.dailyVTHOBurn))}</span>
       </div>
       <div>
         <span className="BottomBar-header">Txs: </span>
-        {numberWithCommas(stats.dailyTransactions)}
+        <span className={numberClass}>{numberWithCommas(stats.dailyTransactions)}</span>
       </div>
       <div>
         <span className="BottomBar-header">Clauses: </span>
-        {numberWithCommas(stats.dailyClauses)}
+        <span className={numberClass}>{numberWithCommas(stats.dailyClauses)}</span>
       </div>
     </div>
   </div>
