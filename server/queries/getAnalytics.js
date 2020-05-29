66573b5d1287686e2a94cb9d70732c7064baa08a
Sylvan Swierkosz <sswierkosz@gmail.com>
2020-05-29 16:17:28 -0700

    adds limit to getAnalytics query

--- server/queries/getAnalytics.js
+++ server/queries/getAnalytics.js
@@ -1,8 +1,9 @@
-module.exports = async function(client) {
+module.exports = async function(client, limit = 30) {
   const uniqueVisits = await client.many(
     `
-      SELECT count(*), date FROM unique_visitors GROUP BY date ORDER BY date;
-    `
+      SELECT count(*), date FROM unique_visitors GROUP BY date ORDER BY date LIMIT $1;
+    `,
+    [limit]
   )
 
   return {
