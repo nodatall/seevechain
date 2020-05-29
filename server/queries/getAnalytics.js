6cd89320b851fbd06ae01a285692ba9d2f6fb2d7
Sylvan Swierkosz <sswierkosz@gmail.com>
2020-05-29 16:35:48 -0700

    orders getAnalyticsQuery by DESC

--- server/queries/getAnalytics.js
+++ server/queries/getAnalytics.js
@@ -1,7 +1,7 @@
 module.exports = async function(client, limit = 30) {
   const uniqueVisits = await client.many(
     `
-      SELECT count(*), date FROM unique_visitors GROUP BY date ORDER BY date LIMIT $1;
+      SELECT count(*), date FROM unique_visitors GROUP BY date ORDER BY date DESC LIMIT $1;
     `,
     [limit]
   )
