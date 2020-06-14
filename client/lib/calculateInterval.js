debabf9af21d32870f02fb16a53304f8424b58ef
Sylvan Swierkosz <sswierkosz@gmail.com>
2020-06-14 09:32:40 -0700

    reduces total interval to 9500

--- client/lib/calculateInterval.js
+++ client/lib/calculateInterval.js
@@ -1,5 +1,5 @@
 export default function calculateInterval(length) {
-  let interval = Math.floor(10000 / length)
+  let interval = Math.floor(9500 / length)
   if (interval > 2000) interval = 2000
   return interval
 }
