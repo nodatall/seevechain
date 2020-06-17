dfa1b42996164ee5aba7cd59e4b56f5cf7f36d0d
Sylvan Swierkosz <sswierkosz@gmail.com>
2020-06-17 08:39:48 -0700

    changes most integers to bigint and returns numbers from get latest

---
+++ migrations/20200617153701-big-ints.js
@@ -0,0 +1,31 @@
+exports.up = db => {
+  return db.runSql(
+    `
+      ALTER TABLE blocks
+      ALTER COLUMN number TYPE bigint,
+      ALTER COLUMN gas_used TYPE bigint;
+
+      ALTER TABLE transactions
+      ALTER COLUMN block_number TYPE bigint,
+      ALTER COLUMN gas TYPE bigint,
+      ALTER COLUMN gas_price_coef TYPE bigint,
+      ALTER COLUMN gas_used TYPE bigint;
+    `
+  )
+}
+
+exports.down = db => {
+  return db.runSql(
+    `
+      ALTER TABLE blocks
+      ALTER COLUMN number TYPE integer,
+      ALTER COLUMN gas_used TYPE integer;
+
+      ALTER TABLE transactions
+      ALTER COLUMN block_number TYPE integer,
+      ALTER COLUMN gas TYPE integer,
+      ALTER COLUMN gas_price_coef TYPE integer,
+      ALTER COLUMN gas_used TYPE integer;
+    `
+  )
+}
