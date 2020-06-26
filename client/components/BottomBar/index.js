887912406b619b24642b7037ea4823e3ed19bf27
Sylvan Swierkosz <sswierkosz@gmail.com>
2020-06-26 01:32:31 -0700

    adjusts sizes of bottom bar depending on screen size

--- client/components/BottomBar/index.js
+++ client/components/BottomBar/index.js
@@ -26,15 +26,15 @@ export default function BottomBar({ stats }) {
   return <div className="BottomBar">
     <div className="BottomBar-wrapper">
       <div>
-        <span className="BottomBar-header">Txs: </span>
+        <span className="BottomBar-header">Txs </span>
         <span className={numberClass}>{numberWithCommas(stats.dailyTransactions) || 0}</span>
       </div>
       <div className="BottomBar-vtho">
-        <span className="BottomBar-header">VTHO Burn: </span>
+        <span className="BottomBar-header">VTHO {window.innerWidth < 400 ? '🔥' : 'Burn'} </span>
         <span className={numberClass}>{numberWithCommas(Math.floor(+stats.dailyVTHOBurn)) || 0}</span>
       </div>
       <div>
-        <span className="BottomBar-header">Clauses: </span>
+        <span className="BottomBar-header">Clauses </span>
         <span className={numberClass}>{numberWithCommas(stats.dailyClauses) || 0}</span>
       </div>
     </div>
