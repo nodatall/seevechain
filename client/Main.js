c19a821bda5649fc326abc8322407f5438e80141
Sylvan Swierkosz <sswierkosz@gmail.com>
2020-06-30 17:29:50 -0700

    uses chartjs instead of chartist

--- client/Main.js
+++ client/Main.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import loadable from '@loadable/component'
+import { Suspense, lazy } from 'preact/compat'
 
 import Visualizer from 'components/Visualizer'
-const Analytics = loadable(() => import('components/Analytics'))
+import Spinner from 'components/Spinner'
+const Analytics = lazy(() => import('components/Analytics'))
 
 import './index.sass'
 
 export default function Main() {
-  if (window.location.pathname === '/analytics') return <Analytics />
+  if (window.location.pathname === '/analytics') {
+    return <Suspense fallback={<Spinner />}>
+      <Analytics />
+    </Suspense>
+  }
   return <Visualizer />
 }
--- client/components/Analytics/index.js
+++ client/components/Analytics/index.js
@@ -3,7 +3,7 @@ import Cookies from 'js-cookie'
 import axios from 'axios'
 import Spinner from 'components/Spinner'
 import { useEffect, useState } from 'preact/hooks'
-import Chart from 'react-chartist'
+import { Line } from 'react-chartjs-2'
 
 import './index.sass'
 
@@ -24,17 +24,56 @@ export default function Analytics() {
     labels.push(date)
     series.push(count)
   })
-  const chartData = { labels, series: [series] }
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: `Visits: ${analytics.uniqueVisits[analytics.uniqueVisits.length - 1].count}`,
+        fill: true,
+        lineTension: 0.1,
+        backgroundColor: '#363c4f',
+        borderColor: '#b3c0f9',
+        borderWidth: 1.5,
+        pointBorderColor: '#b3c0f9',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: series,
+      }
+    ]
+  }
 
   const options = {
-    width: '1000px',
-    height: '500px',
-    showArea: true,
+    maintainAspectRatio: false,
+    scales: {
+      yAxes: [{
+        gridLines: {
+          color: 'grey',
+          lineWidth: .5,
+        },
+        ticks: {
+          fontColor: '#bfbfc9',
+        },
+      }],
+      xAxes: [{
+        gridLines: {
+          color: 'grey',
+          lineWidth: .5,
+        },
+        ticks: {
+          fontColor: '#bfbfc9',
+        },
+      }]
+    }
   }
 
   return <div className="Analytics">
-    <p>Today: {analytics.uniqueVisits[analytics.uniqueVisits.length - 1].count}</p>
-    <p>Last {analytics.uniqueVisits.length} days:</p>
-    <Chart data={chartData} options={options} type="Line" responsiveOptions />
+    <div><Line data={data} options={options} /></div>
   </div>
 }
