b0da067be1b4ac6f355f46a429ef43b05ab34d92
Sylvan Swierkosz <sswierkosz@gmail.com>
2020-07-02 14:03:43 -0700

    prepends DNVGL to MyStory and MyCare

--- client/lib/knownAddresses.js
+++ client/lib/knownAddresses.js
@@ -1,13 +1,13 @@
 const KNOWN_ADDRESSES = {
   '0x505b95e128e403634fe6090472485341905fc0f9': `Yunnan Pu'er Tea`,
-  '0xba6b65f7a48636b3e533205d9070598b4faf6a0c': 'DNV-GL',
+  '0xba6b65f7a48636b3e533205d9070598b4faf6a0c': 'DNVGL',
   '0xbb763cea82127548c465f6ad83a297f292e5c2fb': 'Reebonz',
   '0xfbc5c4e371164e3f1dc5d1760a98f3d227ba7e3b': 'Reebonz',
   '0x9ee753d070c1fd42d715e951bd8d5441e6c7d052': 'Reebonz',
   '0x1a2f8fc8e821f46d6962bb0a4e06349a3ad4cf33': 'Walmart China',
   '0xbe7a61b0405fdfbaae28c1355cd53c8affc1c4b0': 'Walmart China',
   '0xc89dcd4b36b5182f974c556408681cd035be18e4': 'FoodGates',
-  '0xecc159751f9aed21399d5e3ce72bc9d4fccb9ccc': 'MyStory',
+  '0xecc159751f9aed21399d5e3ce72bc9d4fccb9ccc': 'DNVGL MyStory',
   '0x9bcb81a9eadd1457ee9729365f9a77d190670ab2': 'Shanghai Gas',
   '0xf9f99f982f3ea9020f0a0afd4d4679dfee1b63cf': 'vexchange.io',
   '0xf306dfc3c4a276ac4c1795c5896e9f4a967341b6': 'realitems.io',
@@ -15,7 +15,7 @@ const KNOWN_ADDRESSES = {
   '0xa9f3c1bd52c3a506cecbb8cbca562ef26c833175': 'Yuhongtai Foods',
   '0x040093ab307f5acb4ae3afb0fb31de0ec46d62f9': 'safehaven.io',
   '0x1111111111111111111111111111111111111111': 've-name.web.app',
-  '0x1Cc13a24b1F73288cc7791C2c8Fd428357405226': 'MyCare',
+  '0x1Cc13a24b1F73288cc7791C2c8Fd428357405226': 'DNVGL MyCare',
 }
 
 export default KNOWN_ADDRESSES
