39e0197292016a670e4ec142b60fc6838ba4636f
Sylvan Swierkosz <sswierkosz@gmail.com>
2020-07-07 21:34:22 -0700

    less transactions are closer together

--- client/lib/calculateInterval.js
+++ client/lib/calculateInterval.js
@@ -1,5 +1,5 @@
 export default function calculateInterval(length) {
-  let interval = Math.floor(9500 / length)
-  if (interval > 2000) interval = 2000
+  let interval = Math.floor(10000 / length)
+  if (interval > 1000) interval = 1000
   return interval
 }
