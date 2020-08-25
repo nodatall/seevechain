33cfbfdbfab1b2ff45a270551717bd2494ddbf5c
Sylvan Swierkosz <sswierkosz@gmail.com>
2020-08-24 21:23:58 -0700

    waits 500ms after getting block to send latest

--- server/lib/dateHelpers.js
+++ server/lib/dateHelpers.js
@@ -1,7 +1,7 @@
 const moment = require('moment')
 
 function oneDayAgo() {
-  const now  = moment()
+  const now = moment()
   return now
     .subtract((+process.env.TIME_DIFFERENCE + +now.format('HH')) % 24, 'hours')
     .subtract(+now.format('mm'), 'minutes')
