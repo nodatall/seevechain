97765d07094f03c59f7f086089fbba40ef89f03c
Sylvan Swierkosz <sswierkosz@gmail.com>
2020-09-14 14:09:04 -0700

    changes Suspense to height 100%

--- client/Main.js
+++ client/Main.js
@@ -9,7 +9,7 @@ import './index.sass'
 
 export default function Main() {
   if (window.location.pathname === '/analytics') {
-    return <Suspense fallback={<div style={{height: '100vh'}}><Spinner /></div>}>
+    return <Suspense fallback={<div style={{height: '100%'}}><Spinner /></div>}>
       <Analytics />
     </Suspense>
   }
