c256a73a0c8bd39bf0aec2396f8eaf1906afefd8
Sylvan Swierkosz <sswierkosz@gmail.com>
2020-10-16 17:04:44 -0700

    only get analytics since beginning of traffic

--- server/queries/getAnalytics.js
+++ server/queries/getAnalytics.js
@@ -1,7 +1,12 @@
 module.exports = async function(client, limit = 360) {
   const uniqueVisits = await client.many(
     `
-      SELECT count(*), date FROM unique_visitors GROUP BY date ORDER BY date DESC LIMIT $1;
+      SELECT count(*), date
+      FROM unique_visitors
+      WHERE timestamp > '2020-05-26'
+      GROUP BY date
+      ORDER BY date DESC
+      LIMIT $1;
     `,
     [limit]
   )
