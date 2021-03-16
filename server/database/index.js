083b97674992d24566ed223b6f7e8045f9b4a77a
Sylvan Swierkosz <sswierkosz@gmail.com>
2021-03-15 17:37:07 -0700

    sets pgp to ssl

--- server/database/index.js
+++ server/database/index.js
@@ -3,5 +3,6 @@ require('../../environment.js')
 const pgp = require('pg-promise')
 
 const db = pgp()
+db.pg.defaults.ssl = true
 const client = db(process.env.DATABASE_URL)
 module.exports = client
